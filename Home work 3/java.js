85bb2679b0cbfed9edff1996510593eff301502a
Baizak-Otorbekov <162703040+Baizak-Otorbekov@users.noreply.github.com>
2024-03-28 18:13:19 +0600

    Add files via upload

---
+++ Home work 3/java.js
@@ -0,0 +1,552 @@
+const aboutMe = {
+    name: 'Baizak',
+    age: 40,
+    lastName: 'Otorbekov',
+    isMaried: true,
+    job: {
+        name: 'MES KG',
+        position: 'Head of CMC',
+        duty: 'coordination of actions in emergency situations, organization of communication between departments, reconnaissance, in addition to a visual overview of the scene of the incident, analysis and preparation of decisions for management'
+    },
+    lang: ['ru', 'kg', 'en', 'turk'],
+    hobby: [
+        {
+            name: 'reading ',
+            isUsefull: true
+        },
+        {
+            name: 'tennis',
+            isUsefull: true
+        },
+        {
+            name: 'smoking',
+            isUsefull: false
+        },
+    ],
+    addres: {
+        stret: 'Valihanova',
+        home: '3b/30',
+        city: 'Bishkek',
+        country: 'KG',
+        mail: 'Baizak.oto@mail.ru',
+        cont: '0551 00 11 00'
+    },
+    children: [
+        {
+            name: 'Izzat',
+            age: 8,
+            lastName: 'Otorbekov',
+            position: 'scoolboy',
+            lang: ['ru', 'kg', 'en'],
+            adress: {
+                street: 'Valihanova',
+                home: '3b/30',
+                city: 'Bishkek',
+            },
+            hobby: [
+                {
+                    name: 'plaing chess',
+                    isUsufull: true
+                },
+                {
+                    name: 'comp games',
+                    isUsufull: false
+                },
+            ]
+        },
+        {
+            name: 'Aiyma',
+            age: 4,
+            lastName: 'Otorbekova',
+            position: 'kindergarden',
+            lang: ['ru', 'kg'],
+            adress: {
+                street: 'Valihanova',
+                home: '3b/30',
+                city: 'Bishkek',
+            },
+        },
+        {
+            name: 'Milana',
+            age: 2,
+            lastName: 'Otorbekova',
+            position: false,
+            lang: 'kg',
+            adress: {
+                street: 'Valihanova',
+                home: '3b/30',
+                city: 'Bishkek',
+            },
+        },
+    ],
+    wife: [
+        {
+            name: 'Kenje',
+            age: 38,
+            lastName: 'Omoeva',
+            lang: ['ru', 'kg', 'en'],
+            job: {
+                name: 'MES KG',
+                position: 'state employe',
+            },
+            adress: {
+                street: 'Valihanova',
+                home: '3b/30',
+                city: 'Bishkek',
+                country: 'KG'
+            },
+            hobby: [
+                {
+                    name: 'reading',
+                    isUsufull: true
+                },
+                {
+                    name: 'cooking',
+                    isUsufull: true
+                },
+            ]
+        },
+    ],
+    parents: [
+        {
+            name: 'Jumabek',
+            age: 65,
+            lastName: 'Otorbekov',
+            relation: 'father',
+            isMaried: true,
+            lang: ['ru', 'kg'],
+            job: 'retired',
+            adress: {
+                street: 'Samsaliev',
+                home: 19,
+                city: 'Belovodsk'
+            },
+            hobby: [
+                {
+                    name: 'reading',
+                    isUsufull: true
+                },
+                {
+                    name: 'running',
+                    isUsufull: true
+                },
+                {
+                    name: 'driving',
+                    isUsufull: true
+                },
+                {
+                    name: 'smoking',
+                    isUsufull: false
+                },
+            ]
+        },
+        {
+            name: 'Nurila',
+            age: 65,
+            lastName: 'Otorbekova',
+            relation: 'mother',
+            isMaried: true,
+            lang: ['ru', 'kg', 'en'],
+            adress: {
+                street: 'Samsaliev',
+                home: 19,
+                city: 'Belovodsk'
+            },
+            job: {
+                name: 'midle school',
+                position: 'teacher',
+                adress: {
+                    street: 'Shkolnaya',
+                    city: 'Belovodsk'
+                },
+            },
+            hobby: [
+                {
+                    name: 'reading',
+                    isUsufull: true
+                },
+                {
+                    name: 'cooking',
+                    isUsufull: true
+                },
+            ]
+        }
+    ],
+    siblings: [
+        {
+            name: 'Azamat',
+            age: 39,
+            lastName: 'Otorbekov',
+            isMaried: true,
+            relation: 'brother',
+            lang: ['ru', 'kg'],
+            job: {
+                name: 'Finance kredit bank',
+                position: 'director',
+                adress: {
+                    street: 'Kounarov',
+                    city: 'Kara-Balta'
+                },
+            },
+            adress: {
+                street: '1st May',
+                home: 128,
+                city: 'Kara-Balta'
+            },
+            hobby: [
+                {
+                    name: 'reading',
+                    isUsufull: true
+                },
+                {
+                    name: 'running',
+                    isUsufull: true
+                },
+                {
+                    name: 'smoking',
+                    isUsufull: false
+                },
+            ],
+            wife: {
+                name: 'Chinara',
+                age: 33,
+                lastName: 'Kurmanalieva',
+                lang: ['ru', 'kg'],
+                job: {
+                    name: 'RSK bank',
+                    position: 'manager'
+                },
+                adress: {
+                    street: '1st May',
+                    home: 128,
+                    city: 'Kara-Balta'
+                },
+                hobby: [
+                    {
+                        name: 'reading',
+                        isUsufull: true
+                    },
+                    {
+                        name: 'cooking',
+                        isUsufull: true
+                    },
+                ]
+            },
+            children: [
+                {
+                    name: 'Akmaral',
+                    age: 10,
+                    lastName: 'Otorbekova',
+                    position: 'scoolgirl',
+                    lang: ['ru', 'kg'],
+                    adress: {
+                        street: '1st May',
+                        home: 128,
+                        city: 'Kara-Balta'
+                    },
+                    hobby: [
+                        {
+                            name: 'plaing chess',
+                            isUsufull: true
+                        },
+                        {
+                            name: 'online games',
+                            isUsufull: false
+                        },
+                    ]
+                },
+                {
+                    name: 'Sezim',
+                    age: 8,
+                    lastName: 'Otorbekova',
+                    position: 'scoolgirl',
+                    lang: ['ru', 'kg'],
+                    adress: {
+                        street: '1st May',
+                        home: 128,
+                        city: 'Kara-Balta'
+                    },
+                    hobby: [
+                        {
+                            name: 'soccer',
+                            isUsufull: true
+                        },
+                        {
+                            name: 'online games',
+                            isUsufull: false
+                        },
+                    ]
+                },
+                {
+                    name: 'Dani',
+                    age: 5,
+                    lastName: 'Otorbekov',
+                    position: false,
+                    lang: ['ru', 'kg'],
+                    adress: {
+                        street: '1st May',
+                        home: 128,
+                        city: 'Kara-Balta'
+                    },
+                    hobby: [
+                        {
+                            name: 'plaing',
+                            isUsufull: true
+                        },
+                        {
+                            name: 'telephone games',
+                            isUsufull: false
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            name: 'Kani',
+            age: 31,
+            lastName: 'Otorbekov',
+            isMaried: true,
+            relation: 'brother',
+            lang: ['ru', 'kg', 'en'],
+            job: {
+                name: 'MES KG',
+                position: 'head of FRT'
+            },
+            adress: {
+                street: 'Samsaliev',
+                home: 19,
+                city: 'Belovodsk'
+            },
+            hobby: [
+                {
+                    name: 'reading',
+                    isUsufull: true
+                },
+                {
+                    name: 'running',
+                    isUsufull: true
+                },
+                {
+                    name: 'smoking',
+                    isUsufull: false
+                },
+            ],
+            wife: {
+                name: 'Merim',
+                age: 27,
+                lastName: 'Omuralieva',
+                lang: ['ru', 'kg'],
+                job: {
+                    name: 'Bakay bank',
+                    position: 'employe'
+                },
+                adress: {
+                    street: 'Samsaliev',
+                    home: 19,
+                    city: 'Belovodsk'
+                },
+                hobby: [
+                    {
+                        name: 'reading',
+                        isUsufull: true
+                    },
+                    {
+                        name: 'cooking',
+                        isUsufull: true
+                    },
+                ]
+            },
+            children: {
+                name: 'Nurchik',
+                age: 2,
+                lastName: 'Otorbekov',
+                position: false,
+                adress: {
+                    street: 'Samsaliev',
+                    home: 19,
+                    city: 'Belovodsk'
+                },
+            },
+        },
+        {
+            name: 'Tolgonai',
+            age: 25,
+            lastName: 'Otorbekova',
+            isMaried: false,
+            relation: 'sister',
+            job: false,
+            lang: ['ru', 'kg'],
+            adress: {
+                street: 'Samsaliev',
+                home: 19,
+                city: 'Belovodsk'
+            },
+            hobby: [
+                {
+                    name: 'reading',
+                    isUsufull: true
+                },
+                {
+                    name: 'running',
+                    isUsufull: true
+                },
+                {
+                    name: 'cooking',
+                    isUsufull: true
+                },
+            ]
+        }
+    ],
+    friend: [
+        {
+            name: 'Uluk',
+            age: 38,
+            lastName: 'Abdykalykov',
+            isMaried: true,
+            lang: ['ru', 'kg', 'uz'],
+            job: {
+                name: 'adcacy',
+                position: 'director',
+                adress: {
+                    street: 'K.Datka',
+                    home: 256,
+                    city: 'Osh'
+                },
+                addres: {
+                    stret: 'Abdykadarova',
+                    home: 123,
+                    city: 'Osh',
+                },
+                hobby: [
+                    {
+                        name: 'online games',
+                        isUsefull: false
+                    },
+                    {
+                        name: 'running',
+                        isUsefull: true
+                    },
+                    {
+                        name: 'reading',
+                        isUsefull: true
+                    },
+                    {
+                        name: 'smoking',
+                        isUsefull: false
+                    },
+                ],
+                children: [
+                    {
+                        name: 'Aidana',
+                        age: 18,
+                        lastName: 'Abdykalykova',
+                        position: 'student',
+                        lang: ['ru', 'kg', 'en'],
+                        isMaried: false,
+                        adress: {
+                            stret: 'Abdykadarova',
+                            home: 123,
+                            city: 'Osh',
+                        },
+                        hobby: [
+                            {
+                                name: 'plaing chess',
+                                isUsufull: true
+                            },
+                            {
+                                name: 'online games',
+                                isUsufull: false
+                            },
+                            {
+                                name: 'reading',
+                                isUsufull: true
+                            },
+                            {
+                                name: 'cooking',
+                                isUsufull: true
+                            },
+                        ]
+                    },
+                    {
+                        name: 'Mars',
+                        age: 15,
+                        lastName: 'Abdykalykova',
+                        position: 'scoolboy',
+                        lang: ['ru', 'kg'],
+                        adress: {
+                            stret: 'Abdykadarova',
+                            home: 123,
+                            city: 'Osh',
+                        },
+                        hobby: [
+                            {
+                                name: 'soccer',
+                                isUsufull: true
+                            },
+                            {
+                                name: 'online games',
+                                isUsufull: false
+                            },
+                        ]
+                    },
+                    {
+                        name: 'Mels',
+                        age: 5,
+                        lastName: 'Abdykalykov',
+                        position: false,
+                        lang: ['ru', 'kg'],
+                        adress: {
+                            stret: 'Abdykadarova',
+                            home: 123,
+                            city: 'Osh',
+                        },
+                        hobby: [
+                            {
+                                name: 'plaing',
+                                isUsufull: true
+                            },
+                            {
+                                name: 'telephone games',
+                                isUsufull: false
+                            },
+                        ]
+                    },
+                ]
+            },
+
+            wife: {
+                name: 'Aizada',
+                age: 35,
+                lastName: 'Abdykalykov',
+                isMaried: true,
+                lang: ['ru', 'kg', 'uz'],
+                job: false,
+                addres: {
+                    stret: 'Abdykadarova',
+                    home: 123,
+                    city: 'Osh',
+                },
+                hobby: [
+                    {
+                        name: 'online games',
+                        isUsefull: false
+                    },
+                    {
+                        name: 'swimming',
+                        isUsefull: true
+                    },
+                    {
+                        name: 'reading',
+                        isUsefull: true
+                    },
+                    {
+                        name: 'sleeping',
+                        isUsefull: false
+                    },
+                ],
+            },
+        },
+    ]
+
+}
+console.log(aboutMe)
